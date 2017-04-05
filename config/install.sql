625fbb696e9c7870cdca5f07d7298861236dbfb8
rnt-oak <r.affentranger@aronet.ch>
2017-04-05 16:48:48 +0200

    DNS tables added

--- config/install.sql
+++ config/install.sql
@@ -1,2 +1,45 @@
 /* used by installer for createing tables in database */
 
+CREATE TABLE IF NOT EXISTS `dns_slaves` (
+  `id` int(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `fqdn` varchar(50) NOT NULL,
+  `ipv4` varchar(39) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/* https://de.wikipedia.org/wiki/SOA_Resource_Record */
+CREATE TABLE IF NOT EXISTS `dns_zones` (
+  `id` int(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `customer` int(11) unsigned NOT NULL COMMENT 'FK customers',
+  `extern` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = intern, 1 = extern',
+  `name` varchar(70) NOT NULL,
+  `ttl` int(10) NOT NULL DEFAULT '600' COMMENT 'default TTL',
+  `primary` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `refresh` int(10) NOT NULL DEFAULT '3600',
+  `retry` int(10) NOT NULL DEFAULT '1800',
+  `expire` int(10) NOT NULL DEFAULT '604800',
+  `minimum` int(10) NOT NULL DEFAULT '86400',
+  `dnssec` text COMMENT 'DNSSEC Informations',
+  `comment` text ,
+  `pending` text,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/* https://de.wikipedia.org/wiki/Resource_Record */
+CREATE TABLE IF NOT EXISTS `dns_records` (
+  `id` int(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `dns_zone` int(11) unsigned NOT NULL COMMENT 'FK dns_zones',
+  `host` varchar(255) NOT NULL,
+  `ttl` int(10) DEFAULT NULL COMMENT 'record TTL',
+  `type` varchar(16) NOT NULL, /* supported records: *,A,AAAA,CNAME,MX,NS,SRV,TXT,hosting,ipssl,redirect */
+  `value` text NOT NULL,
+  /* start HWS only */
+  `hosting` int(11) NOT NULL COMMENT 'FK hostings; if ssl activated FK ip/ssl', 
+  `redirect` text NOT NULL COMMENT 'URL for redirect',
+  /* end HWS only */
+  `comment` text,
+  `pending` text,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
---
+++ models/DnsZones.php
@@ -0,0 +1,131 @@
+<?php
+/**
+* @copyright Copyright (c) ARONET GmbH (https://aronet.swiss)
+* @license AGPL-3.0
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License, version 3,
+* as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License, version 3,
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*
+*/
+
+namespace RNTForest\dns\models;
+
+use Phalcon\Validation;
+
+use RNTForest\core\interfaces\JobServerInterface;
+use RNTForest\core\interfaces\PendingInterface;
+use RNTForest\core\libraries\PendingHelpers;
+
+class DnsZones extends \RNTForest\core\models\ModelBase implements JobServerInterface, PendingInterface
+{
+
+    
+    /* properties, setter and getter */
+    
+    
+    
+
+    /**
+    * Initialize method for model.
+    */
+    public function initialize()
+    {
+        $this->setup(array('notNullValidations'=>false));
+        $this->setup(array('virtualForeignKeys'=>false));
+
+        $this->belongsTo("customers_id",'RNTForest\core\models\Customers',"id",array("alias"=>"Customers", "foreignKey"=>true));
+
+        
+    }
+    
+    /**
+    * Validations and business logic
+    *
+    * @return boolean
+    */
+    public function validation()
+    {
+        // get params from session
+        $session = $this->getDI()->get("session")->get("DnsZonesValidator");
+        $op = $session['op'];
+
+        $validator = $this->generateValidator($op,$vstype);
+        if(!$this->validate($validator)) return false;
+        
+        // business logic
+        /* do something usefull here */
+
+        return true;
+    }
+
+    
+    /**
+    * generates validator for VirtualServer model
+    * 
+    * return \Phalcon\Validation $validator
+    * 
+    */
+    public static function generateValidator($op,$vstype){
+        
+        // validator
+        $validator = new Validation();
+
+        // name
+        $message = self::translate("dnszone_name_required");
+        $validator->add('name', new PresenceOfValidator([
+            'message' => $message
+        ]));        
+
+        /* show in other validators für hints */
+        
+        return $validator;
+    }
+    
+    /**
+    * Add a PendingToken to the PendingEntity.
+    * For conversion of a PendingString to a PendingToken use PendingHelpers::convert Method.
+    * 
+    * @param array $pendingToken a valid PendingToken
+    */
+    public function addPending($pendingToken){
+        $pendingArray = json_decode($this->pending,true);
+        $pendingArray[] = $pendingToken;        
+        $this->pending = json_encode($pendingArray);
+        $this->save();
+    }
+    
+    /**
+    * Remove a PendingToken from the PendingEntity.
+    * For conversion of a PendingString to a PendingToken use PendingHelpers::convert Method.
+    * 
+    * @param array $pendingToken a valid PendingToken
+    */
+    public function removePending($pendingToken){
+        $pendingArray = json_decode($this->pending,true);
+        $this->pending = json_encode(PendingHelpers::removePendingTokenInPendingArray($pendingToken,$pendingArray));
+        $this->save();
+    }
+    
+    /**
+    * Checks if a PendingEntity is pending representative to the given PendingToken.
+    * If no PendingToken is given it will return true if any PendingToken is in the PendingEntity. 
+    * 
+    * @param array $pendingToken (optional) a valid PendingToken 
+    * @return boolean 
+    */
+    public function isPending($pendingToken=''){
+        $pendingArray = json_decode($this->pending,true);
+        return PendingHelpers::checkForPendingTokenInPendingArray($pendingToken,$pendingArray);
+    }
+    
+    
+}
